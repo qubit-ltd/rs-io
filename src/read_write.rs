b3dff944ceea342e6d984c5b113011aae34ad88f
Haixing Hu <starfish.hu@gmail.com>
2026-05-04 02:25:44 +0800

    chore(ci): align CI configuration

--- src/read_write.rs
+++ src/read_write.rs
@@ -7,7 +7,10 @@
  *    Licensed under the Apache License, Version 2.0.
  *
  ******************************************************************************/
-use std::io::{Read, Write};
+use std::io::{
+    Read,
+    Write,
+};
 
 /// Object-safe capability trait for values that can be both read and written.
 ///
